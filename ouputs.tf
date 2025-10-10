ddc1d2d22797b5d66f21958ef9330da88713994d
Prerana <vprerana23@gmail.com>
2025-10-10 19:08:54 +0530

    Reset main branch to empty

--- ouputs.tf
+++
@@ -1,8 +0,0 @@
-output "ip"{
-    value = aws_instance.flask_express.public_ip
-}
-
-output "s3_bucket_name" {
-  description = "S3 bucket for .env files"
-  value       = aws_s3_bucket.env_bucket.bucket
-}
